440018e547bdba7f0ceddeeb6396d177db130093
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-06-20 11:28:53 +0530

    Add files via upload

---
+++ src/General/General.js
@@ -0,0 +1,11 @@
+// export const API = 'http://localhost:4000';
+export const API = "https://hackathon2-backend.vercel.app";
+
+export const LinkStyling = {
+  textDecoration: "none",
+  color: "inherit",
+};
+
+export const githubLink = "https://github.com/Rahul-R-1796";
+export const twitterLink = "https://twitter.com";
+export const linkedinLink = "https://www.linkedin.com";
